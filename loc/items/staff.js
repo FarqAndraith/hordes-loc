45556b84f4ef922d0d2a8d994fa1f9942b98ee2a
FarqAndraith <49627616+FarqAndraith@users.noreply.github.com>
2019-04-15 18:09:52 -0500

    Update staff.js
    Added Portuguese

--- loc/items/staff.js
+++ loc/items/staff.js
@@ -2,12 +2,13 @@ export default [
   {
     name       : {en: 'Broken Twig' },
     description: {
-      en: 'Weapon forged by the greatest blacksmith of all the land. At least thats what we told little Timmy.',
+      en: 'Weapon forged by the greatest blacksmith of all the land. At least that\'s what we told little Timmy.',
       hi: 'क्षेत्र के सबसे महान लोहार द्वारा बनाया गया हथियार। नन्हें टिम्मी से तो हमने यही कहा।',
       ru: 'Оружие, выкованное величайшим кузнецом из всей земли. По крайней мере, так мы сказали маленькому Тимми.',
       es: 'Este es un palo normal. Pero ¿estás seguro que solo es un palo normal?',
       de: 'Die Waffe wurde vom größten Schmied des Landes geschmiedet. Zumindest haben wir dem kleinen Timmy das gesagt.',
-      fr: 'Une arme forgée par le plus grand forgeron du pays. Du moins, c\'est ce qu\'on a dit au petit Timmy.'
+      fr: 'Une arme forgée par le plus grand forgeron du pays. Du moins, c\'est ce qu\'on a dit au petit Timmy.',
+      pt: 'Arma forjada pelo maior ferreiro de toda a terra. Pelo menos foi o que dissemos ao pequeno Timmy.'
     }
   },
 
@@ -18,7 +19,8 @@ export default [
       ru: 'Отчаянные ситуации требуют отчаянных мер.',
       es: 'Has roto su palo. Está bien, todavía puedes usarlo.',
       de: 'Verzweifelte Situationen erfordern verzweifelte Maßnahmen.',
-      fr: 'Les situations désespérées exigent des mesures désespérées.'
+      fr: 'Les situations désespérées exigent des mesures désespérées.',
+      pt: 'Situações desesperadas exigem medidas desesperadas.'
     }
   },
 
@@ -29,7 +31,8 @@ export default [
       ru: 'Потрепанная метла украдена у мага. Корявый и костлявый.',
       es: 'Un palo de escoba desaliñado robado de un mago. Nudoso y huesudo.',
       de: 'Dreckiger Besenstiel, der einem Magier gestohlen wurde. Knurrig und knöchern.',
-      fr: 'Un balai négligé volé à un mage. Noueux et osseux.'
+      fr: 'Un balai négligé volé à un mage. Noueux et osseux.',
+      pt: 'Vassoura de vassoura roubada de um mago. Gnarradas e ossudas.'
     }
   },
 
@@ -41,7 +44,8 @@ export default [
       ru: 'Палочка, вырезанная из ветвей древнего дуба. Обладает глубокой силой.',
       es: 'Una varita cortada de las ramas de un arce anciano. Tiene gran poder canadiense.',
       de: 'Ein Zauberstab, der aus den Zweigen einer kastanienbraunen Eiche geschnitten wurde. Durchdrungen von tiefer Kraft.',
-      fr: 'Baguette coupée dans les branches d\'un chêne d\'ancien. Doté d\'une puissance profonde.'
+      fr: 'Baguette coupée dans les branches d\'un chêne d\'ancien. Doté d\'une puissance profonde.',
+      pt: 'Uma varinha cortada dos ramos de um antigo carvalho. Infundido com um poder profundo.'
     }
   },
 
@@ -52,7 +56,8 @@ export default [
       ru: 'Мистическое оружие, светящееся загадочной сущностью.',
       es: 'Arma mística que brilla con una esencia misteriosa.',
       de: 'Mystische Waffe, die mit einer mysteriösen Essenz angestrahlt wird.',
-      fr: 'Une arme mystique qui brille d\'une essence mystérieuse.'
+      fr: 'Une arme mystique qui brille d\'une essence mystérieuse.',
+      pt: 'Uma arma mística que brilha com uma essência misteriosa.'
     }
   },
 
@@ -63,7 +68,8 @@ export default [
       ru: 'Ничего особенного в этом оружии, только бедро лича.',
       es: 'El rey liche no necesitaba su fémur, así que lo usé para hacer una varita.',
       de: 'Nichts Besonderes an dieser Waffe, nur der Oberschenkelknochen eines Lichs.',
-      fr: 'Cette arme n\'a rien d\'extravagant, juste le fémur d\'une liche.'
+      fr: 'Cette arme n\'a rien d\'extravagant, juste le fémur d\'une liche.',
+      pt: 'Nada de especial nesta arma, apenas o fémur de um líquen.'
     }
   },
 
@@ -74,7 +80,8 @@ export default [
       ru: 'Длинная палочка с синим мерцающим драгоценным камнем на короне.',
       es: 'Varita larga, con una joya azul brillante en su corona.',
       de: 'Langer Stab, mit einem blau schimmernden Edelstein an der Krone.',
-      fr: 'Longue baguette, avec une gemme bleue chatoyante à sa couronne.'
+      fr: 'Longue baguette, avec une gemme bleue chatoyante à sa couronne.',
+      pt: 'Vara comprida com uma gema azul cintilante na sua coroa.'
     }
   },
 
@@ -85,7 +92,8 @@ export default [
       ru: 'Темно-фиолетовый посох. Совершенно таинственно в его создании, но приятно смотреть на него.',
       es: 'El cayado de un pastor. Solo costó como noventa cobres y una gallina.',
       de: 'Dunkelvioletter Stab. Ziemlich mysteriös in seiner Entstehung, aber schön anzusehen.',
-      fr: 'Bâton violet foncé. Assez mystérieux dans sa fabrication, mais agréable à regarder.'
+      fr: 'Bâton violet foncé. Assez mystérieux dans sa fabrication, mais agréable à regarder.',
+      pt: 'Cajado roxo escuro. Bastante misterioso na sua criação, mas agradável de se ver.'
     }
   },
 
@@ -96,39 +104,43 @@ export default [
       ru: 'Создан из изумрудов украденных у горных гномов.',
       es: 'Hay una enana en las montañas, se llama Esmeralda. Ella solo hace varitas de esmeralda por algún razón.',
       de: 'Gebaut aus Smaragden, die den Bergweibchen gestohlen wurden.',
-      fr: 'Construit à partir d\'émeraudes volées aux nains des montagnes.'
+      fr: 'Construit à partir d\'émeraudes volées aux nains des montagnes.',
+      pt: 'Construído a partir de esmeraldas roubadas dos anões das montanhas.'
     }
   },
 
   {
     name       : {en: 'Sapphire Staff' },
     description: {
-      en: 'Strange.... the staff seems.... triangular....',
-      ru: 'Странно.... посох кажется.... треугольным....',
-      es: 'Extraño.... el bastón parece.... triangular...',
-      de: 'Seltsam.... der Stab scheint.... dreieckig.....',
-      fr: 'Bizarre.... le bâton semble.... triangulaire....'
+      en: 'Strange... the staff seems... triangular...',
+      ru: 'Странно... посох кажется... треугольным...',
+      es: 'Extraño... el bastón parece... triangular...',
+      de: 'Seltsam... der Stab scheint... dreieckig...',
+      fr: 'Bizarre... le bâton semble... triangulaire...',
+      pt: 'Estranho... o bastão parece... triangular...'
     }
   },
 
   {
     name       : {en: 'Frozen Greatstaff' },
     description: {
-      en: 'Eccentric rod with a pale blue stone embedded inside....',
-      ru: 'Эксцентриковый стержень с бледно-голубым камнем, встроенным внутрь ....',
+      en: 'Eccentric rod with a pale blue stone embedded inside...',
+      ru: 'Эксцентриковый стержень с бледно-голубым камнем, встроенным внутрь ...',
       es: 'Finalmente, ¡una varita de nieve para el mago de invierno!',
-      de: 'Exzentrische Stange mit einem hellblauen Stein, der innen eingebettet ist.......',
-      fr: 'Tige excentrique avec une pierre bleu pâle incrustée à l\'intérieur......'
+      de: 'Exzentrische Stange mit einem hellblauen Stein, der innen eingebettet ist...',
+      fr: 'Tige excentrique avec une pierre bleu pâle incrustée à l\'intérieur...',
+      pt: 'Vara excêntrica com uma pedra azul pálida embutida no interior...'
     }
   },
 
   {
     name       : {en: 'Infernal Staff' },
     description: {
-      en: 'Constructed by unknwon blacksmiths residing in fabled valleys.',
+      en: 'Constructed by unknown blacksmiths residing in fabled valleys.',
       ru: 'Построен неизвестными кузнецами, живущими в сказочных долинах.',
       es: 'Construido por herreros desconocidos que residen en valles legendarios.',
-      fr: 'Confectionné par des forgerons inconnus résidant dans des vallées légendaires.'
+      fr: 'Confectionné par des forgerons inconnus résidant dans des vallées légendaires.',
+      pt: 'Construído por ferreiros desconhecidos que residem em vales lendários.'
 
     }
   },
@@ -140,7 +152,8 @@ export default [
       ru: 'Его красота призывает тебя ..... убивать ..... завоевывать ....',
       es: '¿Qué dice la belleza de este bastón? Te dice: "...mátalos....conquista el mundo....será divertido..."',
       de: 'Seine Schönheit ruft nach dir, zu töten, zu erobern...',
-      fr: 'Sa beauté vous interpelle.....à tuer.....à conquérir...'
+      fr: 'Sa beauté vous interpelle.....à tuer.....à conquérir...',
+      pt: 'A sua beleza chama-te... para matares... para conquistares...'
     }
   },
 
@@ -151,7 +164,8 @@ export default [
       ru: 'Посох, украденный у ангелов. Они все еще охотятся за этим.',
       es: 'Robaste el arzobispo y tomaste su báculo de oro. Ahora los ángeles te cazan.',
       de: 'Ein Stab, der von den Engeln gestohlen wurde. Sie sind immer noch auf der Jagd danach.',
-      fr: 'Un bâton volé aux anges. Ils sont toujours à sa recherche.'
+      fr: 'Un bâton volé aux anges. Ils sont toujours à sa recherche.',
+      pt: 'Um bastão roubado aos anjos. Eles ainda estão caçando por isso.'
     }
   },
 
@@ -162,7 +176,8 @@ export default [
       ru: 'Грубый в своей внешности, но могущественный по его ядро ....',
       es: 'Crudo en su aspecto, pero poderoso en su núcleo....',
       de: 'Grob in seinem Aussehen, aber mächtig in seinem Kern....',
-      fr: 'Rudimentaire en apparence, mais puissant dans son essence....'
+      fr: 'Rudimentaire en apparence, mais puissant dans son essence....',
+      pt: 'Cruel na sua aparência, mas poderoso no seu núcleo...'
     }
   },
 
@@ -173,7 +188,8 @@ export default [
       ru: 'Нам говорят, что его ядро ​​от сердца ведьмы. Никто не нашел труп. Или сам посох.',
       es: 'Se nos dice que su núcleo es del corazón de una bruja. Nadie ha encontrado el cadáver. O el propio personal.',
       de: 'Uns wurde gesagt, dass sein Kern aus dem Herzen einer Hexe stammt. Niemand hat die Leiche gefunden. Oder das Personal selbst.',
-      fr: 'On nous dit que son coeur est celui d\'une sorcière. Personne n\'en a retrouvé le cadavre. Ou la baguette elle-même.'
+      fr: 'On nous dit que son coeur est celui d\'une sorcière. Personne n\'en a retrouvé le cadavre. Ou la baguette elle-même.',
+      pt: 'Dizem-nos que o núcleo é do coração de uma bruxa. Ninguém encontrou o cadáver. Ou o próprio cajado.'
     }
   },
 
@@ -184,7 +200,8 @@ export default [
       ru: 'Это оружие настолько скрытно, что те, кто владеют им, убили всех, кто о нем знает.',
       es: 'Se dicen que nadie se queda vivo para contar de este arco, pero... ¿de dónde viene las historias?',
       de: 'Diese Waffe ist so geheimnisvoll, dass die wenigen, die sie führen, alle getötet haben, die von ihr wissen.',
-      fr: 'Cette arme est si secrète que les quelques personnes qui la manie ont tué tous ceux qui la connaissent.'
+      fr: 'Cette arme est si secrète que les quelques personnes qui la manie ont tué tous ceux qui la connaissent.',
+      pt: 'Esta arma é tão secreta que os poucos que a empunham mataram todos os que a conhecem.'
     }
   }
 
