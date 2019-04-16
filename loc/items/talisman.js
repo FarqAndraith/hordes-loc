2b65caba45925d47397a184727bf25c2eb6779ba
FarqAndraith <49627616+FarqAndraith@users.noreply.github.com>
2019-04-15 19:18:57 -0500

    Update talisman.js
    Added Portuguese

--- loc/items/talisman.js
+++ loc/items/talisman.js
@@ -6,7 +6,8 @@ export default [
       ru: 'Пахнет так плохо, я удивляюсь, что ты даже поднял его.',
       es: 'Huele tan mal que me pregunto por qué lo recogiste.',
       de: 'Riecht so übel, dass ich mich frage, warum du es überhaupt aufgehoben hast.',
-      fr: 'Ça sent si mauvais que je me demande pourquoi tu l\'as ramassé.'
+      fr: 'Ça sent si mauvais que je me demande pourquoi tu l\'as ramassé.',
+      pt: 'Cheira tão mal que me pergunto porque é que o apanhaste.'
     }
   },
 
@@ -17,7 +18,8 @@ export default [
       ru: 'Брось его на солнце, и он скажет тебе, куда идти.',
       es: 'Tíralo al sol y te dirá adónde ir.',
       de: 'Werfen Sie es in die Sonne und es wird Ihnen sagen, wohin Sie gehen müssen.',
-      fr: 'Jette-le au soleil et il te dira où aller.'
+      fr: 'Jette-le au soleil et il te dira où aller.',
+      pt: 'Atira-o ao sol e ele dir-te-á para onde ir.'
     }
   },
 
@@ -28,7 +30,8 @@ export default [
       ru: 'Удивительно, но он не пахнет рыбой. Просто не показывай ему пресную воду.',
       es: 'Sorprendentemente, no huele a pescado. No lo muestres a agua dulce.',
       de: 'Überraschenderweise riecht es nicht nach Fisch. Zeigen Sie es einfach nicht im Süßwasser.',
-      fr: 'Étonnamment, il ne sent pas le poisson. Ne lui montre pas de l\'eau de douce.'
+      fr: 'Étonnamment, il ne sent pas le poisson. Ne lui montre pas de l\'eau de douce.',
+      pt: 'Surpreendentemente, não cheira a peixe. Só não mostre água doce.'
     }
   },
 
@@ -39,7 +42,8 @@ export default [
       ru: 'Не могу произнести, не могу использовать.',
       es: 'No puedo pronunciarlo, ni puedo usarlo.',
       de: 'Ich kann es nicht aussprechen, ich kann es nicht benutzen.',
-      fr: 'Si je ne peux pas le prononcer, je ne peux pas l\'utiliser.'
+      fr: 'Si je ne peux pas le prononcer, je ne peux pas l\'utiliser.',
+      pt: 'Não consigo pronunciá-lo, não consigo usá-lo.'
     }
   },
 
@@ -50,8 +54,8 @@ export default [
       ru: 'Разорванный между мирами, этот талисман позволяет переходить между вселенные.',
       es: 'Arrancado de entre mundos, este talismán permite la transición entre reinos.',
       de: 'Aus den Welten gerissen, ermöglicht dieser Talisman den Übergang zwischen den Welten.',
-      fr: 'Déchiré entre les mondes, ce talisman permet une transition entre les royaumes.'
-
+      fr: 'Déchiré entre les mondes, ce talisman permet une transition entre les royaumes.',
+      pt: 'Rasgado entre mundos, este talismã permite a transição entre reinos.'
     }
   }
 
